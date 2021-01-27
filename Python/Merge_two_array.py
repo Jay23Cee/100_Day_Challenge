4cd51c01fe5653340ae8952aeaab8f01f8301477
jay23cee <jayceexxiii@gmail.com>
2021-01-27 01:10:43 -0800

    Merging two arrays

---
+++ Python/Merge_two_array.py
@@ -0,0 +1,42 @@
+def mergeArrays(arr1, arr2, n1,n2):
+    i=0;
+    j=0;
+    k=0;
+    arr3 = [None] * (n1+n2)
+
+    
+    while i < n1 and j < n2:
+    
+        if arr1[i] > arr2[j]:
+            arr3[k] = arr2[j]
+            j= j+1
+            k =k+1
+        elif arr1[i] < arr2[j]:
+            arr3[k] = arr1[i]
+            i =i+1
+            k =k+1
+        else:
+            arr3[k] = arr2[i]
+            j= j+1
+            i =i+1
+            k =k+1
+    
+    while i < n1:
+        arr3[k] = arr1[i]
+        k =k+1
+        i =k+1
+    
+    while j<n2:
+        arr3[k] = arr1[j]
+        k =k+1
+        i =i+1
+    
+    print(arr3, end = " " )
+  
+# Driver code 
+arr1 = [1, 3, 5, 7,9] 
+n1 = len(arr1) 
+  
+arr2 = [2, 4, 6, 8] 
+n2 = len(arr2) 
+mergeArrays(arr1, arr2, n1, n2); 
