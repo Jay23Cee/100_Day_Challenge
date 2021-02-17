79c486ee29c6a4185d055c781ca5e10ec36bc9ec
jay23cee <jayceexxiii@gmail.com>
2021-02-16 22:18:53 -0800

    removed my debugging print

--- Python/Clone_Linked_List.py
+++ Python/Clone_Linked_List.py
@@ -23,67 +23,63 @@ def traverse(head):
     print("null")
 
 #Function to clone a linked list having random pointers
-# Function to clone a linked list having random pointers
 def cloneLinkedList(head):
- 
-    ## 1. Create a duplicate of each node of the original linked list
- 
-    # traverse the original linked list
+
+    
+    ##1. create a duplicate of each node of the original linked list
+    #traverse the original linked list
     curr = head
     while curr:
-        # take a pointer to the next node in the original list
+        #take a pointer to the next node in the original list
         next = curr.next
- 
-        # duplicate each node of the linked list
+
+        #duplicate each node of the linked list
         dup = Node(curr.data)
- 
-        # associate each duplicate node with the next child of the original node
+
+        #associate each duplicate node with the next child of the original
         curr.next = dup
         dup.next = next
- 
-        # advance to the next node in the original list
+
+        #advance to the next node in the original list
         curr = next
- 
-    ## 2. Update the random pointers of the duplicated nodes
- 
+
+    ##2 . update the random pointers of the duplicated nodes
+
     # traverse the modified list
     curr = head
     while curr:
- 
-        # if a random pointer for the original node exists, set it for the clone
+        #if a random pointer for the original node exists, set it for the clone
         if curr.random:
             curr.next.random = curr.random.next
- 
-        # advance to the next node in the original list
+        
+        #Advance to the current next node in the original list
         curr = curr.next.next
- 
-    ## 3. Extract the duplicate nodes from the modified list
- 
-    # construct a dummy node whose next pointer points to the head
-    # of the cloned linked list
+    ##3 extract the duplicate nodes from the modified list
+
+    #construct a dummy node whose next pointer points to the head
+    #of the cloned linked list
     dummy = Node(-1)
- 
-    # maintain a tail node for the clone
+
+    #maintain a tail node for the clone
     tail = dummy
- 
-    # traverse the modified list
+    
+    #traverse the modified list
     curr = head
     while curr:
-        # take a pointer to the next node in the original list
+        #take a pointer to the next node in the original list
         next = curr.next.next
- 
-        # extract the duplicate
+
+        #extract the duplicate
         dup = curr.next
         tail.next = dup
-        tail = dup
- 
-        # restore the original linked list
+        tail=dup
+
+        #restore the original linked list
         curr.next = next
- 
-        # advance to the next node in the original list
-        curr = next
- 
-    # return head node of the cloned list
+
+        #advance to the next node in the original list
+        curr =next
+    #return head node of the cloned list
     return dummy.next
 
 if __name__ == '__main__':
