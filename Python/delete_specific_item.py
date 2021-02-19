9ea7d39465b76753f02f4da7f4efc9af4fd2bb7b
jay23cee <jayceexxiii@gmail.com>
2021-02-18 23:32:30 -0800

    completed Deleting a node from a double linked list

--- Python/delete_specific_item.py
+++ Python/delete_specific_item.py
@@ -28,5 +28,63 @@ class doubly_linked_list(object):
     def iter(self):
         #Iterate the list
         current = self.head
+        while current:
+            item_val = current.value
+            current = current.next
+            yield item_val
+    
+    def print_forward(self):
+        for node in self.iter():
+            print(node)
+
+    def search_item(self,val):
+        for node in self.iter():
+            if val == node:
+                return True
+        return False
+    
+    def delete(self,value):
+        #Delete a specific item
+        current = self.head
+        node_deleted = False
+        if current is None:
+            node_deleted = False
         
+        elif current.value == value:
+            self.head = current.next
+            self.head.prev = None
+            node_deleted = True
+
+        else:
+            while current:
+                if current.value == value:
+                    current.prev.next = current.next
+                    current.next.prev = current.prev
+                    node_deleted = True
+                current = current.next
+        
+        if node_deleted:
+            self.count -= 1
+
+items = doubly_linked_list()
+items.append_item('PHP')
+items.append_item('Python')
+items.append_item('C#')
+items.append_item('C++')
+items.append_item('Java')
+items.append_item('JavaScript')
+items.append_item('SQL')
+
+print("Original List:")
+items.print_forward()
+
+items.delete("Java")
+items.delete("Python")
+print("\nList after deleting two items:")
+items.print_forward()
+
+        
+            
+
+
             
