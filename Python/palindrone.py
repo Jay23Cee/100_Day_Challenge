1aeb83066e2894b5172f6d21540fe0a59daafcd1
jay23cee <jayceexxiii@gmail.com>
2021-02-19 22:38:31 -0800

    Corrections

--- Python/palindrone.py
+++ Python/palindrone.py
@@ -1,19 +1,37 @@
-txt = "A plan, has a man"
+#Check if linkedlistis a palindrome
 
+#Node class
+class Node:
+    #constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-def is_palindrone(s):
-    new_str = []
-    for x in s:
-        if not x.isalpha():
-            continue
-        new_str.append(x.islower())
-        
-    new_str = "".join(new_str)
-    return is_palindrone(new_str)
+class LinkedList:
+    #function to initialize head
+    def __init__(self):
+        self.head = None
+    
+    #A utility function to check if str is palindrome
+    def isPalindromeUtil(self, string):
+        return (string == string[::-1])
 
+    #Returns true if string formed by linked list is
+    #Palindrome
+    def isPalindrome(self):
+        node = self.head
 
-
-
-
-
-print(is_palindrone(txt))
+        #Append all nodes to form a string
+        temp = []
+        while (node is not None):
+            temp.append(node.data)
+            node = node.next
+        string = "".join(temp)
+        return self.isPalindromeUtil(string)
+    
+    #Utility function to print the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print (temp.data),
+            temp = temp.next
