d61011e73a4acd9ccb741a36cd2f5479c27ac49f
jay23cee <jayceexxiii@gmail.com>
2021-02-26 21:45:43 -0800

    fix the first node to delete

--- Python/delete_binary_search_tree_node.py
+++ Python/delete_binary_search_tree_node.py
@@ -2,40 +2,65 @@
 
 class TreeNode(object):
     def __init__(self, x):
-        self.val = x
+        self.data = x
         self.left = None
         self.right = None
     
-def delete_Node(root, key):
-    #If root doesn't exist , just return it
-    if not root:
+def minValueNode(node):
+    current = node
+
+    #loop down to find the leftmost leaf
+    while(current.left is not None):
+        current = current.left
+    return current
+
+#Given a binary search tree and a key, this function
+#delete the data and returns the new host
+
+def deleteNode(root, data):
+    #Base case
+    if root is None:
         return root
-    #Find the node in the left subtree if key value is less than root value
-    if root.val > key:
-        root.left = delete_Node(root.left, key)
-    #Find the node in right subtree if key value is greater than root value,
-    elif root.val < key:
-        root.right = delete_Node(root.right, key)
-    #Delete the node if root.value == key
+    
+    #If the key to be deleted
+    #is smaller than the root's
+    #key then it lies in left subtree
+
+    if data < root.data:
+        root.left = deleteNode(root.left,data)
+
+    #if the Kye to be delete
+    #is greater than the root data
+    #then it lies in the right subtree
+    elif(data > root.data):
+        root.right = deleteNode(root.right, data)
+    
+    #if key is same as root's key, then this is the node
+    # to be deleted
     else:
-        #If there is no right children delete the node and new root would be root.left
-        if not root.right:
-            return root.left
-    #If there is no left children delete the node and new root would be root.right
-        if not root.left:
-            return root.right
-    
-    # If both left and right children exist in the node replace its value with
-    # the minminum value in the right subtree. Now delete the minimum node 
-    # in the right subtree
-        temp_val = root.right
-        mini_val = temp_val.val
-        while temp_val.left:
-            temp_val = temp_val.left
-            mini_val= temp_val.val
-    
-    #Delete the minimum node in right subtree
-        root.right = delete_Node(root.right, root.val)
+
+        #Node with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+
+        # node with two children:
+        #get the inorder successor
+        # (smallest in the right subtree)
+        temp = minValueNode(root.right)
+
+        #Copy the inorder successor's
+        #content to this node
+        root.data = temp.data
+
+        #Delete the inorder successor
+        root.right = deleteNode(root.right, temp.data)
     
     return root
 
@@ -43,7 +68,7 @@ def delete_Node(root, key):
 def preOrder(node):
     if not node:
         return
-    print(node.val)
+    print(node.data)
     preOrder(node.left)
     preOrder(node.right)
 
@@ -56,7 +81,7 @@ root.left.right = TreeNode(4)
 root.left.right.left = TreeNode(7)
 print("Original node:")
 print(preOrder(root))
-result = delete_Node(root, 3)
+root = deleteNode(root, 5)
 print("After deleting specified node:")
-print(preOrder(result))
+print(preOrder(root))
 
