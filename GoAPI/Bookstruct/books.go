2c7f98e792acf217ae3776a5e74375a42ae23df0
jay23cee <jayceexxiii@gmail.com>
2021-03-12 23:34:26 -0800

    added gorm to struc

--- GoAPI/Bookstruct/books.go
+++ GoAPI/Bookstruct/books.go
@@ -1,35 +1,36 @@
 package Bookstruct
 
-type Getter interface{
+import "github.com/jinzhu/gorm"
+
+type Getter interface {
 	GetAll() []Book
 }
 
-type Adder interface{
+type Adder interface {
 	Add(book Book)
-
 }
 
-type Book struct{
-	Title string `json:"title`
+type Book struct {
+	gorm.Model
+	Title  string `json:"title`
 	Author string `json:"author`
-	Time string `json:"time`
-
+	Time   string `json:"time`
 }
 
-type Repo struct{
+type Repo struct {
 	Books []Book
 }
 
-func New() *Repo{
+func New() *Repo {
 	return &Repo{
 		Books: []Book{},
 	}
 }
 
-func(r *Repo) Add(book Book){
+func (r *Repo) Add(book Book) {
 	r.Books = append(r.Books, book)
 }
 
-func(r *Repo) GetAll() []Book{
+func (r *Repo) GetAll() []Book {
 	return r.Books
 }
